0fc123dee4f3f62e59522434aaf8778c0beac50b
Erdem A <erdemarasads@gmail.com>
2026-04-05 17:58:52 +0300

    Yeni script baştan yüklendi

--- docker/start.sh
+++ docker/start.sh
@@ -16,13 +16,20 @@ http {
         listen $LISTEN_PORT;
         root /var/www/html;
         index index.php index.html;
+
         location / {
             try_files \$uri \$uri/ /index.php?\$query_string;
         }
+
+        location /mextonmadmin/ {
+            try_files \$uri \$uri/ /mextonmadmin/index.php?\$query_string;
+        }
+
         location ~ \.php\$ {
             fastcgi_pass 127.0.0.1:9000;
             fastcgi_index index.php;
             fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+            fastcgi_read_timeout 10;
             include fastcgi_params;
         }
     }
