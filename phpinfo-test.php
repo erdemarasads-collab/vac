5dba146e868fbb659a07e4fb22c3ebe6f9145da4
Erdem A <erdemarasads@gmail.com>
2026-04-05 17:03:11 +0300

    Yeni script baştan yüklendi

--- phpinfo-test.php
+++ phpinfo-test.php
@@ -1,7 +1,5 @@
 <?php
-echo "PHP Version: " . phpversion() . "<br>";
-echo "PDO drivers: ";
-print_r(PDO::getAvailableDrivers());
-echo "<br>";
-echo "pdo_mysql loaded: " . (extension_loaded('pdo_mysql') ? 'YES' : 'NO') . "<br>";
-echo "mysqli loaded: " . (extension_loaded('mysqli') ? 'YES' : 'NO') . "<br>";
+echo "MYSQLHOST: " . (getenv('MYSQLHOST') ?: 'NULL getenv') . "<br>";
+echo "MYSQLHOST _ENV: " . ($_ENV['MYSQLHOST'] ?? 'NULL _ENV') . "<br>";
+echo "MYSQLHOST _SERVER: " . ($_SERVER['MYSQLHOST'] ?? 'NULL _SERVER') . "<br>";
+echo "MYSQLPORT: " . (getenv('MYSQLPORT') ?: 'NULL') . "<br>";
