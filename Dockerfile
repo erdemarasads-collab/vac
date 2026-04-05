8b0f3feb74e33d647cf220708271b003f6db1471
Erdem A <erdemarasads@gmail.com>
2026-04-05 16:37:36 +0300

    Yeni script baştan yüklendi

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,10 @@ RUN docker-php-ext-install pdo pdo_mysql mysqli
 
 RUN apk add --no-cache nginx
 
-COPY docker/nginx.conf /etc/nginx/nginx.conf
-
 COPY . /var/www/html/
-
-RUN chown -R www-data:www-data /var/www/html
-
 COPY docker/start.sh /start.sh
-RUN chmod +x /start.sh
 
-EXPOSE 80
+RUN chown -R www-data:www-data /var/www/html \
+    && chmod +x /start.sh
 
 CMD ["/start.sh"]
--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
+set -e
+
+LISTEN_PORT=${PORT:-80}
+
+cat > /etc/nginx/nginx.conf << EOF
+worker_processes 1;
+events {
+    worker_connections 1024;
+}
+http {
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+    server {
+        listen ${LISTEN_PORT};
+        root /var/www/html;
+        index index.php index.html;
+        location / {
+            try_files \$uri \$uri/ /index.php?\$query_string;
+        }
+        location ~ \.php$ {
+            fastcgi_pass 127.0.0.1:9000;
+            fastcgi_index index.php;
+            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+            include fastcgi_params;
+        }
+    }
+}
+EOF
+
 php-fpm &
 sleep 2
-envsubst '${PORT}' < /etc/nginx/nginx.conf > /tmp/nginx.conf
-nginx -c /tmp/nginx.conf -g "daemon off;"
+exec nginx -g "daemon off;"
